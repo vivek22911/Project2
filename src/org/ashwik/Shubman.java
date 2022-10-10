74508482681346e65ea30761b3e9678ffd58dfdd
vivek22911 <antony.vivek980@gmail.com>
2022-10-10 19:43:37 +0530

    project done

---
+++ src/org/ashwik/Shubman.java
@@ -0,0 +1,9 @@
+package org.ashwik;
+
+public class Shubman {
+        public static void main(String[] args) {
+			System.out.println("Surya");
+			System.out.println("Kohli");
+			System.out.println("Tendulkar");
+		}
+}
